482cc9cdf3d6c314ded37dac2e49b1d8f9056016
Franck Kouame <franckk07@gmail.com>
2023-10-25 08:05:32 +0000

    responsive v2

--- main.js
+++ main.js
@@ -23,4 +23,37 @@ listService.addEventListener("mouseout",()=>{
 listService.style.display = "none";
 })
 
+// /affficher masquer suite de texte aprropo
+
+function toggleText() {
+    var texteCache = document.getElementById("texteCache");
+    var suite = document.getElementById("suite");
+
+    if (texteCache.style.display === "none") {
+        texteCache.style.display = "block";
+        suite.textContent = "Masquer";
+    } else {
+        texteCache.style.display = "none";
+        suite.textContent = "..Lire la suite";
+    }
+}
+
+
+// /afficher cacher menue lateral
+
+const menueBar = document.querySelector("#menue-bar");
+const menueFermer = document.querySelector("#croix");
+const menuelateral = document.querySelector("#menue-lateral");
+
+menueBar.addEventListener("click",()=>{
+menueBar.style.display = "none";
+menueFermer.style.display = "initial";
+menuelateral.style.left = "0";
+})
+
+menueFermer.addEventListener("click",()=>{
+    menueBar.style.display = "initial";
+    menueFermer.style.display = "none";
+    menuelateral.style.left = "-100%";
+})
 
